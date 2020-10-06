ee40b472b5d646b23afab849f9a921aefc37d399
Tim Mattison <tim@mattison.org>
2020-10-06 09:19:38 -0400

    Attempting to get the generated class info from a JAR file instead of the default class loader

--- src/main/java/com/aws/samples/lambda/servlet/automation/GeneratedClassFinder.java
+++ src/main/java/com/aws/samples/lambda/servlet/automation/GeneratedClassFinder.java
@@ -1,18 +1,29 @@
 package com.aws.samples.lambda.servlet.automation;
 
 import com.aws.samples.lambda.servlet.LambdaWebServletProcessor;
+import io.vavr.control.Try;
 
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.jar.JarFile;
 import java.util.stream.Collectors;
 
 public class GeneratedClassFinder {
-    public List<GeneratedClassInfo> getGeneratedClassList() {
-        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(LambdaWebServletProcessor.RESOURCE_FILE);
+    public List<GeneratedClassInfo> getGeneratedClassList(JarFile jarFile) {
+        return Try.of(() -> jarFile.getJarEntry(LambdaWebServletProcessor.RESOURCE_FILE))
+                .mapTry(jarFile::getInputStream)
+                .map(this::innerGetGeneratedClassList)
+                .get();
+    }
 
+    private List<GeneratedClassInfo> getGeneratedClassList() {
+        return innerGetGeneratedClassList(this.getClass().getClassLoader().getResourceAsStream(LambdaWebServletProcessor.RESOURCE_FILE));
+    }
+
+    private List<GeneratedClassInfo> innerGetGeneratedClassList(InputStream inputStream) {
         if (inputStream == null) {
             return new ArrayList<>();
         }
