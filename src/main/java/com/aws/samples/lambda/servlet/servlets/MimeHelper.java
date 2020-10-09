f145fca7c5ed5f95d74cfb31152a17290c472195
Tim Mattison <tim@mattison.org>
2020-10-09 16:55:53 -0400

    Updated interface for future features

--- src/main/java/com/aws/samples/lambda/servlet/servlets/MimeHelper.java
+++ src/main/java/com/aws/samples/lambda/servlet/servlets/MimeHelper.java
@@ -1,7 +1,14 @@
 package com.aws.samples.lambda.servlet.servlets;
 
+import io.vavr.control.Option;
+
 import java.io.File;
+import java.io.InputStream;
 
 public interface MimeHelper {
     String detect(File file);
+
+    String detect(InputStream file);
+
+    String detect(Option<String> filename, InputStream file);
 }
